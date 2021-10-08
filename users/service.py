f484a2f13b748d592d84ed61e7d0b20132448150
Akash Jha <ajha@fleetstudio.com>
2021-10-08 13:55:24 +0530

    Modified the Child DB anf table creation procedure

--- users/service.py
+++ users/service.py
@@ -26,9 +26,9 @@ class UserService:
         )
         my_new_cursor=new_db.cursor()
         my_new_cursor.execute("CREATE TABLE Tokens (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,clientID INTEGER(10) NOT NULL, shopURL VARCHAR(150) NOT NULL, accessToken VARCHAR(255) NOT NULL, validity date NOT NULL)")
-        my_new_cursor.execute("CREATE TABLE Reward_Points (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,customerID INTEGER(10), customerEmail VARCHAR(150) NOT NULL, orderNo INTEGER NOT NULL, orderValue INTEGER NOT NULL, campaignID INTEGER NOT NULL,pointsRewarded INTEGER NOT NULL,status enum('M','F') NOT NULL, dateCreated date NOT NULL)")
-        my_new_cursor.execute("CREATE TABLE Total_Points (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,customerID INTEGER(10), customerEmail VARCHAR(150), totalPointsEarned INTEGER NOT NULL, totalPointsRedeemed INTEGER NOT NULL, totalPoints INTEGER NOT NULL, dateUpdated date NOT NULL)")
-        my_new_cursor.execute("CREATE TABLE Redeem_History (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,customerID INTEGER(10), customerEmail VARCHAR(150), orderNo INTEGER NOT NULL, orderValue INTEGER NOT NULL, campaignID INTEGER NOT NULL, pointsUsed INTEGER NOT NULL, equivalentValue INTEGER NOT NULL, status enum('M','F') NOT NULL, dateCreated date NOT NULL)")
+        my_new_cursor.execute("CREATE TABLE Reward_Points (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,customerID INTEGER(10) NOT NULL, customerEmail VARCHAR(150) NOT NULL, orderNo INTEGER NOT NULL, orderValue INTEGER NOT NULL, campaignID INTEGER NOT NULL,pointsRewarded INTEGER NOT NULL,status enum('M','F') NOT NULL, dateCreated date NOT NULL)")
+        my_new_cursor.execute("CREATE TABLE Total_Points (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,customerID INTEGER(10) NOT NULL, customerEmail VARCHAR(150) NOT NULL, totalPointsEarned INTEGER NOT NULL, totalPointsRedeemed INTEGER NOT NULL, totalPoints INTEGER NOT NULL, dateUpdated date NOT NULL)")
+        my_new_cursor.execute("CREATE TABLE Redeem_History (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,customerID INTEGER(10) NOT NULL, customerEmail VARCHAR(150) NOT NULL, orderNo INTEGER NOT NULL, orderValue INTEGER NOT NULL, campaignID INTEGER NOT NULL, pointsUsed INTEGER NOT NULL, equivalentValue INTEGER NOT NULL, status enum('M','F') NOT NULL, dateCreated date NOT NULL)")
         return {"status":create_user}
 
 user_service = UserService()
